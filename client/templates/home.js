a7f0f1ec71b0d0f1a0dbc237abccd8484de40d4f
deeCodeMonkey <deidra108@gmail.com>
2018-01-27 21:15:46 -0700

    done

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,3 +1,6 @@
+Meteor.subscribe('images');
+Meteor.subscribe('imageInfo');
+
 Template.home.helpers({
     images: function () {
         return Images.find({}, { sort: {uploadedAt: -1}})
--- server/publish.js
+++ server/publish.js
@@ -1,1 +1,7 @@
-﻿
+﻿Meteor.publish('images', function () {
+    return Images.find();
+});
+
+Meteor.publish('imageInfo', function () {
+    return ImageInfo.find();
+});
